517823032f24be6e45c0e660a5ee1f41537a88fc
jiahongfei <jiahongfeinew@163.com>
2016-02-17 16:43:55 +0800

    增加上下翻页的同时下面列表点击事件的控制

--- app/src/main/java/com/jhf/viewgroup/demo/ArticleCommentLayout.java
+++ app/src/main/java/com/jhf/viewgroup/demo/ArticleCommentLayout.java
@@ -56,7 +56,7 @@ public class ArticleCommentLayout extends LinearLayout implements
 		mPullToRefreshListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				if(articleDetailsViewGroup.isScrollerFinished()){
+				if(!articleDetailsViewGroup.isScrollerFinished()){
 					return ;
 				}
 				Toast.makeText(mContext, "position : " + position, Toast.LENGTH_SHORT).show();
